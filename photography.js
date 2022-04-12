99a2f504cd41083d7fd6bd429b68e187d2e63a24
Honesh Rai Pareek <honeshraipareek@gmail.com>
2022-04-12 13:52:42 +0530

    NewStart

--- photography.js
+++
@@ -1,8 +0,0 @@
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1, delay: 1 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=0.85");
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
-tl.to(".landing", {opacity: 0, x:"100%", duration:3})
